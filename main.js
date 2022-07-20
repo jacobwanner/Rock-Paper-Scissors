abd0a7d9101cd91d7a08a9550d8c7317d2daa920
Jacob Wanner <106052324+jacobwanner@users.noreply.github.com>
2022-07-19 21:39:17 -0400

    Replacing Initial Files
    Replacing as I solved the getComputerChoice function in my code editor app.

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-console.log("Hello World!")
 
-function computerChoice() {
-    Math.floor((Math.random()*3) +1);
-    return;
+function getComputerChoice() {
+   let answer = Math.floor(Math.random()*3 +1);  
+   if (answer === 1) {
+        return "Rock"
+    } else if (answer === 2) {
+        return "Paper"
+    } else if (answer === 3) {
+        return "Scissors"
+    } else {
+        return "Error BEEP BOP"
+    } ;
 }
 
-computerChoice
+console.log(getComputerChoice());
+
+
+
+
+
+// let userChoice = prompt("Write Rock Paper or Scissors:");
+
+// function playRound(userChoice, getComputerChoice) {
+//     if ()
+// }
