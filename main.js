488bd01094f75fbf9d780a12a68f9f85ab38d5bc
Jacob Wanner <106052324+jacobwanner@users.noreply.github.com>
2022-07-24 19:41:40 -0400

    Rewrote main.js - now fully playable round
    There was a duplicate of the function getComputerChoice() so one of those were taken out.
    I changed the strings returned from the getComputerChoice() function to be in lowercase which was causing the playRound function not to read properly.
    added to the first console.log() of the computers answer so now it outputs a string of what stating what is being output.
    I also changed what is being output in the first console.log() to be the same answer the computer is generating to what is being run in the playRound() function.

--- main.js
+++ main.js
@@ -1,48 +1,39 @@
 function getComputerChoice() {
-   let answer = Math.floor(Math.random()*3 +1);  
-   if (answer === 1) {
-        return "Rock"
-    } else if (answer === 2) {
-        return "Paper"
-    } else if (answer === 3) {
-        return "Scissors"
-   let computerAnswer = Math.floor(Math.random()*3 +1);  
-   if (computerAnswer === 1) {
-        return "rock"
-    } else if (computerAnswer === 2) {
-        return "paper"
-    } else if (computerAnswer === 3) {
-        return "scissors"
-    } else {
-        return "Error BEEP BOP"
-    } ;
-    }
-}
-let userSelection = prompt("Write rock paper or scissors:");
-let computerSelection = getComputerChoice();
-let computerWinner = computerSelection + " beats " + userSelection + ". You Lose!";
-let userWinner = userSelection + " beats " + computerSelection + ". You win!";
+    let answer = Math.floor(Math.random()*3 +1);  
+    if (answer === 1) {
+         return "rock"
+     } else if (answer === 2) {
+         return "paper"
+     } else if (answer === 3) {
+         return "scissors"
+     }
+ }
 
-function playRound(userSelection, computerSelection) {
-    if (userSelection == "rock" && computerSelection == "rock" ) {
-        return "Tie game. It's a draw!"
-    } else if (userSelection === "rock" && computerSelection === "paper" ) {
-        return computerWinner
-    } else if (userSelection === "rock" && computerSelection === "scissors" ) {
-        return userWinner
-    } else if (userSelection === "paper" && computerSelection === "rock" ) {
-        return winnerComputer
-    } else if (userSelection === "paper" && computerSelection === "scissors" ) {
-        return userWinner
-    } else if (userSelection === "scissors" && computerSelection === "rock" ) {
-        return computerWinner
-    } else if (userSelection === "scissors" && computerSelection === "paper" ) {
-        return userWinner
-    } else {
-        return "Mission Failed. We'll get e'm next time."
-    }
-}
-
-console.log(getComputerChoice());
-
-console.log(playRound(userSelection, computerSelection)); 
+ let userSelection = prompt("Write rock paper or scissors:");
+ let computerSelection = getComputerChoice();
+ let computerWinner = computerSelection + " beats " + userSelection + ". You Lose!";
+ let userWinner = userSelection + " beats " + computerSelection + ". You win!";
+ 
+ function playRound(userSelection, computerSelection) {
+     if (userSelection == "rock" && computerSelection == "rock" ) {
+         return "Tie game. It's a draw!"
+     } else if (userSelection === "rock" && computerSelection === "paper" ) {
+         return computerWinner
+     } else if (userSelection === "rock" && computerSelection === "scissors" ) {
+         return userWinner
+     } else if (userSelection === "paper" && computerSelection === "rock" ) {
+         return winnerComputer
+     } else if (userSelection === "paper" && computerSelection === "scissors" ) {
+         return userWinner
+     } else if (userSelection === "scissors" && computerSelection === "rock" ) {
+         return computerWinner
+     } else if (userSelection === "scissors" && computerSelection === "paper" ) {
+         return userWinner
+     } else {
+         return "Mission Failed. We'll get e'm next time."
+     }
+ }
+ 
+ console.log("The computer chose " + computerSelection);
+ 
+ console.log(playRound(userSelection, computerSelection)); 
