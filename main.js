e4f08a21c5d51e9185933476979692ae16de3b3c
Jacob Wanner <106052324+jacobwanner@users.noreply.github.com>
2022-07-26 13:52:33 -0400

    Game function now carries out multiple rounds.
    I still have a problem in the code where when the loop for the function game() will not retrieve a new input for the functions getNewUserChoice and getNewComputerChoice and then use the new inputs as the second round of the loop.

--- main.js
+++ main.js
@@ -1,61 +1,75 @@
-function getComputerChoice() {
+function getNewComputerChoice() {
     let answer = Math.floor(Math.random()*3 +1);  
     if (answer === 1) {
-         return "rock"
-     } else if (answer === 2) {
-         return "paper"
-     } else if (answer === 3) {
-         return "scissors"
-     }
- }
+        return "rock"
+    } else if (answer === 2) {
+        return "paper"
+    } else if (answer === 3) {
+        return "scissors"
+    }
+}
 
- let userSelection = prompt("Write rock paper or scissors:");
- let computerSelection = getComputerChoice();
- let computerWinner = computerSelection + " beats " + userSelection + ". You Lose!";
- let userWinner = userSelection + " beats " + computerSelection + ". You win!";
- let tieRound = "Tie game. It's a draw!";
- function playRound(userSelection, computerSelection) {
-     if (userSelection === computerSelection) {
-         return tieRound
-     } else if (userSelection === "rock" && computerSelection === "paper" ) {
-         return computerWinner
-     } else if (userSelection === "rock" && computerSelection === "scissors" ) {
-         return userWinner
-     } else if (userSelection === "paper" && computerSelection === "rock" ) {
-         return userWinner
-     } else if (userSelection === "paper" && computerSelection === "scissors" ) {
-         return computerWinner
-     } else if (userSelection === "scissors" && computerSelection === "rock" ) {
-         return computerWinner
-     } else if (userSelection === "scissors" && computerSelection === "paper" ) {
-         return userWinner
-     } else {
-         return "Mission Failed. We'll get e'm next time."
-     }
- }
- 
-//  let roundResult= playRound(userSelection, computerSelection);
- console.log("The computer chose " + computerSelection);
- console.log(playRound(userSelection, computerSelection)); 
+function getNewUserChoice() {
+    return prompt("Write rock paper or scissors:").toLowerCase();
+}
 
-// let amountOfRounds = prompt("How many rounds for this game?");
+let amountOfRounds = prompt("How many rounds for this game?");
+let userSelection = getNewUserChoice();
+let computerSelection = getNewComputerChoice();
+let computerWinner = "The computer wins this round.";
+let userWinner = "You win this round.";
+let tieRound = "Tie game. It's a draw!";
+let userScore = 0;
+let computerScore = 0;
 
-//  function game(playRound) {
-//     for (let round=0; round<amountOfRounds; round++) {
-//         if (roundResult === computerWinner) {
-//             console.log("Computer won")
-//         } else if (roundResult === userWinner) {
-//             console.log("User won")
-//         } else if (roundResult === tieRound) {
-//             console.log("Draw")
-//         } else console.log("Atleast the loop works")
-//         userSelection
-//         getComputerChoice();
-//         round++
-//     }
-//  }
-// game(playRound())
+function playRound(userSelection, computerSelection) {
+    if (userSelection === computerSelection) {
+        console.log(tieRound)
+    } else if (userSelection === "rock" && computerSelection === "paper" ) {
+        console.log(computerWinner)
+        computerScore++;
+    } else if (userSelection === "rock" && computerSelection === "scissors" ) {
+        console.log(userWinner)
+        userScore++;
+    } else if (userSelection === "paper" && computerSelection === "rock" ) {
+        console.log(userWinner)
+        userScore++
+    } else if (userSelection === "paper" && computerSelection === "scissors" ) {
+        console.log(computerWinner)
+        computerScore++;
+    } else if (userSelection === "scissors" && computerSelection === "rock" ) {
+        console.log(computerWinner)
+        computerScore++;
+    } else if (userSelection === "scissors" && computerSelection === "paper" ) {
+        console.log(userWinner)
+        userScore++
+    } else {
+        console.log("Mission Failed. We'll get e'm next time.")
+    }
+}
 
-// "The computer wins this round. Total score: "  +"/"+ ;
-// "You win this round. Total score: "  +"/"+ ;
-//Let endOfGame =  "Game over!" + (who wins) +"!"+ " Final Score: "  +"/"+ ;
+// let endOfGameTie =  "Game over!" + tieRound + "!" + " Final Score: " + userScore + "-" + computerScore;
+// let endOfGameWinner = "Game over! You Win!"+ " Final Score: " + userScore + "-"+ computerScore;
+// let endOfGameLoser = "Game over! You Lose!"+ " Final Score: " + userScore + "-"+ computerScore;
+
+function game() {
+    for (let i=0; i<amountOfRounds; i++){
+        getNewUserChoice();
+        getNewComputerChoice();
+        console.log("Your choice: " + userSelection);
+        console.log("The computer chose " + computerSelection);
+        playRound(userSelection, computerSelection); 
+        console.log("Your score: " + userScore + " Computer score: " + computerScore);
+    }
+    // if (userScore === computerScore){
+    //     return endOfGameTie;
+    // } else if (userScore > computerScore){
+    //     return endOfGameWinner;
+    // } else if (userScore < computerScore){
+    //     return endOfGameLoser;
+    // } else {
+    //     return "didnt quite work";
+    // }
+}
+
+game()
